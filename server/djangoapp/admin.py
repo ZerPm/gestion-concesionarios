bc7dc40940eb6d90562d7c3551766a1475311355
ZerPm <christian.manco3@gmail.com>
2025-03-08 14:29:00 -0500

    Initial Commit

---
+++ server/djangoapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+
+# CarMakeAdmin class with CarModelInline
+
+# Register models here
